a8cda848b38b8c8b6f905b66ffa2d6f967d6f3cb
NgocNB <jade.ngocnguyenbao@gmail.com>
2020-07-14 22:15:07 +0700

    Check commit notification on

--- webdriver/testng/Topic_01_Check_environment.java
+++ webdriver/testng/Topic_01_Check_environment.java
@@ -42,6 +42,11 @@ public class Topic_01_Check_environment {
 	public void TC_04_LoginFormDisplayed() {
 		//Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
 	}
+	
+	@Test
+	public void TC_05_LoginFormDisplayed() {
+		//Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	}
 
 	@AfterClass
 	public void afterClass() {
